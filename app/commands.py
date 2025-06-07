a835c96df9b2a2bc0805e0c53d5489edc18fe8e3
Asad Ali <asadali.asif.1@gmail.com>
2025-06-07 16:06:37 +0100

    Stage: The exit builtin

---
+++ app/commands.py
@@ -0,0 +1,2 @@
+
+COMMANDS = {"exit"}
--- app/main.py
+++ app/main.py
@@ -1,12 +1,28 @@
 import sys
 
+from commands import COMMANDS
+
 def REPL():
     while True:
         sys.stdout.write("$ ")
 
         # Wait for user input
         command = input()
-        print(f"{command}: command not found")
+
+        parts = command.strip().split(" ")
+
+        if parts[0] not in COMMANDS:
+            print(f"{command}: command not found")
+            continue
+
+        if parts[0] == "exit":
+            if len(parts) > 1:
+                exit_code = parts[1]
+            else:
+                exit_code = 0
+            sys.exit(exit_code)
+        
+
 
 def main():
     REPL()
